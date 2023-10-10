afa1ce7492e128edcdadca342bbddf25315379a8
arielrecto <arielrecto29@gmail.com>
2023-10-10 23:47:06 +0800

    chore:add authentication

--- todolistapp/urls.py
+++ todolistapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.signup, name="register"),
+    path('store_user', views.store_user, name="store_user"),
+    path('login/', views.signin, name="login"),
+    path('auth_user', views.auth_user, name="auth_user"),
+    path('logout', views.signoff, name="signoff"),
     path('create/', views.create, name="create"),
     path('store', views.store, name="store"),
     path('edit/<str:todo_id>/', views.edit, name="edit"),
--- todolistapp/views.py
+++ todolistapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.db import models
 from .models import Todo
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def home (request) :
@@ -12,16 +15,63 @@ def home (request) :
  
     return  render(request, 'home.html', {'todos' : _todos})
 
+
+# authentication
+
+def signup(request):
+    return render(request, 'pages/auth/register.html')
+
+def store_user(request):   
+    User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('email'), password=request.POST.get('password'))
+    messages.success(request, "Register Success!")
+    return redirect(to='home')
+
+def signin(request):
+    
+    if request.user.is_authenticated:
+        return render(request, 'pages/home.html')
+    return render(request, 'pages/auth/login.html')
+
+
+def auth_user(request):
+   
+    user = authenticate(request, username=request.POST.get('username'), password=request.POST.get('password'))
+    if user is not None :
+        login(request, user)
+        return redirect(to='home')
+    else:
+        return redirect(to='login')
+
+
+def signoff(request):
+    logout(request)
+    return redirect(to='home')
+
+
+#todos
+
 def create(request):
-    return  render(request, 'pages/todos/create.html')
+    if not request.user.is_authenticated:
+        return redirect(to='login')
+    
+    print(request.user.is_authenticated)
+   
+    return render(request, 'pages/todos/create.html')
 
 def store(request):
+    
+    if not request.user.is_authenticated:
+        return redirect(to='login')
+    
     if request.method == 'POST' :
         Todo.objects.create(title=request.POST.get('title'), description=request.POST.get('description'))
         return redirect(to="home")
     return render(request, 'pages/todos/create.html')
 
 def edit(request, todo_id):
+    if not request.user.is_authenticated:
+        return redirect(to='login')
+    
     _todo = Todo.objects.get(id=todo_id)
     return render(request, 'pages/todos/edit.html', {'todo' : _todo})
 
@@ -41,4 +91,6 @@ def delete(request, todo_id):
         return redirect(to='home')
     messages.error(request, 'something wrong')
     return redirect(to='home')          
-        
+
+
+       
